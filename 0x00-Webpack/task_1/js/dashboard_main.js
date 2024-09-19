59cc72e63b80f03dc9106d2f550f9195e99c630a
SohaibHegazy <sohaib.mohamed@hotmail.com>
2024-09-19 02:15:50 -0700

    modify

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,20 @@
+import $ from 'jquery';
+import { debounce } from 'lodash';
+
+$(document).ready(function() {
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<button id="start-btn">Click here to get started</button>');
+  $('body').append('<p id="count"></p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
+
+  let count = 0;
+
+  function updateCounter() {
+    count++;
+    $('#count').text(`${count} clicks on the button`);
+  }
+
+  $('#start-btn').on('click', debounce(updateCounter, 300));
+});
+
