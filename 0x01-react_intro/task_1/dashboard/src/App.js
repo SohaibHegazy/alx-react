a28425ebf3cb506beffac1f4d3b661d666670ebf
SohaibHegazy <sohaib.mohamed@hotmail.com>
2024-09-21 12:10:28 -0700

    modify

---
+++ 0x01-react_intro/task_1/dashboard/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import holbertonLogo from './holberton-logo.png';
+import { getFullYear, getFooterCopy } from './utils';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={holbertonLogo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+      </body>
+      <footer className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(false)}</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
+
