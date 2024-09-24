a04c6136e3e0373be76ae8d680be4511d94f957d
SohaibHegazy <sohaib.mohamed@hotmail.com>
2024-09-24 16:56:43 -0700

    modify

---
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,52 @@
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+/** @type {import('webpack').Configuration} */
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, "dist"), // Updated from contentBase
+        },
+        hot: true,
+        compress: true,
+        port: 8564,
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                use: "babel-loader",
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/
+            },
+            {
+                use: ["style-loader", "css-loader"],
+                test: /\.css$/i
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    "file-loader",
+                    {
+                        loader: "image-webpack-loader",
+                        options: {
+                            bypassOnDebug: true, // webpack@1.x
+                            disable: true, // webpack@2.x and newer
+                        },
+                    },
+                ],
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".js", ".jsx", ".json"]
+    },
+    devtool: "inline-source-map",
+}
+
